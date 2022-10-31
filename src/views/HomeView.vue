48ad4e3b5eb2f0d755146691cef1d096483bc105
Jo Matsumaru <hatunehimeka@outlook.jp>
2022-10-31 20:12:43 +0900

    Vue.js初期設定

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import List from '../components/List.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <List />
   </main>
 </template>
